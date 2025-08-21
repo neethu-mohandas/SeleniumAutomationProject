1efa8ea6cb174dfb7a46944eb1a45d212d4e119d
neethumdas <DELL@192.168.1.43>
2025-08-21 17:28:17 +0530

    Final commit

--- AutomationCourse/src/test/java/seleniumBasics/NavigationCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/NavigationCommands.java
@@ -4,9 +4,9 @@ public class NavigationCommands extends Base{
 	
 	public void verifyCommands() {
 		
-		driver.navigate().to("https://www.amazon.in/");	
-		driver.navigate().back();
-		driver.navigate().forward();
+		driver.navigate().to("https://www.amazon.in/");	//navigate from obsqura(given in Base class) to amazon application
+		driver.navigate().back(); //navigate back to obsqura
+		driver.navigate().forward(); //navigate to amazon
 		driver.navigate().refresh();
 		
 	}
